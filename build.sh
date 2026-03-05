18ae5c954943a4b5950817ae6739024c9ae7e27f
Kin <chikin.tam@proton.me>
2026-03-05 01:02:23 -0500

    modify script to run in container

--- build.sh
+++ build.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
-cmake -B build -S . -DJANA_DIR=$JANA_HOME/lib/JANA/cmake
-cmake --build build -j 8
+# modify image pth accordingly
+USER=$(whoami)
+IMAGE="/lustre24/expphy/volatile/eic/$USER/images/jana2root.sif"
+BUILD_LOCAL=false
+
+while getopts "l" opt; do
+  case $opt in
+    l)
+      BUILD_LOCAL=true
+      ;;
+  esac
+done
+
+
+if [ "$BUILD_LOCAL" = true ]; then
+    echo "Building locally..."
+    cmake -B build -S . -DJANA_DIR=$JANA_HOME/lib/JANA/cmake
+    cmake --build build -j 8
+else
+    if [ ! -f "$IMAGE" ]; then
+        echo "Image not found at $IMAGE. Cannot build."
+        exit 1
+    fi
+    echo "Using Singularity image at $IMAGE"
+    singularity exec \
+        --bind $(pwd) \
+        $IMAGE \
+        bash -c "
+        cmake -B build -S .
+        cmake --build build -j 8
+        "   
+fi
--- run.sh
+++ run.sh
@@ -1,11 +1,55 @@
 #!/bin/bash
 
-# Sample run script for RecoClusterVTP plugin. Adjust the parameters as needed.
+set -e
 
-jana -Pplugins=RecoClusterVTP \
+USER=$(whoami)
+WORKDIR=$(pwd)
+
+# modify image pth accordingly
+IMAGE="/lustre24/expphy/volatile/eic/$USER/images/jana2root.sif"
+RUN_LOCAL=false
+
+while getopts "l" opt; do
+  case $opt in
+    l)
+      RUN_LOCAL=true
+      ;;
+  esac
+done
+
+# Sample run script for RecoClusterVTP plugin. Adjust the parameters as needed.
+INPUT="/lustre24/expphy/volatile/hallc/nps/nps-ana/wf_test/ROOTfiles/nps_hms_coin_4599_0_1_-1.root"
+JANA_CMD="jana \
+    -Pplugins=RecoClusterVTP \
     -Pvtp_config_file=database/jlog/nps_run_4599_vtp_config.csv \
     -Pvme_config_file=database/jlog/nps_run_4599_vme_config.csv \
     -Pnps:geo_config_file=database/geo/channel_map.csv \
-    -Preplay_source:max_events=10000 \
+    -Preplay_source:max_events=1000 \
     -Pjana:plugin_path=./build/plugins/RecoClusterVTP \
-    "/lustre24/expphy/volatile/hallc/nps/nps-ana/wf_test/ROOTfiles/nps_hms_coin_4599_0_1_-1.root"
+    ${INPUT}"
+    
+if [ "$RUN_LOCAL" = true ]; then
+    echo "Running locally..."
+    $JANA_CMD
+
+else
+
+    if [ ! -f "$IMAGE" ]; then
+        echo "Image not found at $IMAGE. Abort."
+        exit 1
+    fi
+
+    echo "Using Singularity image at $IMAGE"
+    singularity exec \
+        --bind ${WORKDIR} \
+        --bind /lustre24 \
+        $IMAGE \
+        bash -c "
+        ${JANA_CMD}
+        "   
+fi
+
+
+
+
+
