0de6dafba41e9b98db062e2dabe29eda2fde8315
Kin <chikin.tam@proton.me>
2026-08-03 01:28:23 -0400

    bug : output fadc_hits in vtp clus factory

--- src/nps/main.cpp
+++ src/nps/main.cpp
@@ -147,7 +147,7 @@ int main(int argc, char *argv[]) {
 		vtpClusterGenerator->AddWiring(
 			"VtpClusterFactory",			 // tag
 			{"fadc_waveforms", "vtp_seeds"}, // inputs
-			{"vtp_clusters"}				 // outputs
+			{"fadc_hits", "vtp_clusters"}	 // outputs
 		);
 		app.Add(vtpClusterGenerator);
 	}
@@ -155,9 +155,9 @@ int main(int argc, char *argv[]) {
 	if (enableWaveformClustering) {
 		auto WaveformClusterGenerator = new JOmniFactoryGeneratorT<nps::clustering::WaveformClusterFactory>();
 		WaveformClusterGenerator->AddWiring(
-			"WaveformClusterFactory",		   // tag
-			{"fadc_waveforms"},				   // inputs
-			{"fadc_hits", "waveform_clusters"} // outputs
+			"WaveformClusterFactory", // tag
+			{"fadc_waveforms"},		  // inputs
+			{"waveform_clusters"}	  // outputs
 		);
 		app.Add(WaveformClusterGenerator);
 	}
