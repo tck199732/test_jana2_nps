158c6ea11faf28860e5fa7367bd9e5e8f936dfdd
Kin <chikin.tam@proton.me>
2026-08-10 03:42:41 -0400

    update output according to cluster output since it containes all hit info.

--- src/nps/io/NpyWriterProcessor.hpp
+++ src/nps/io/NpyWriterProcessor.hpp
@@ -55,7 +55,6 @@ public:
 
 class VtpClusterNpyWriteProcessor : public BaseNpyWriterProcessor {
 	Input<nps::fadc_waveform> m_fadc_waveforms{this, {"fadc_waveforms", JEventLevel::None, true}};
-	Input<nps::fadc_hit> m_fadc_hits{this, {"input_fadc_hits", JEventLevel::None, true}};
 	Input<nps::cluster> m_vtp_clusters{this, {"vtp_clusters", JEventLevel::None, true}};
 	Service<nps::geo::NpsGeometryService> m_service_geometry{this};
 
@@ -68,66 +67,68 @@ public:
 	void WriteEntries(const JEvent &event, const std::filesystem::path &output_dir) override {
 
 		const auto &fadc_waveforms = m_fadc_waveforms();
-		const auto &fadc_hits = m_fadc_hits();
 		const auto &vtp_clusters = m_vtp_clusters();
 
-		// for convenience, waveforms are stored as [nhits][nsamples]
-		//  i.e. data could be repeated for multiple hits
-		std::vector<double> waveform_data(fadc_hits.size() * m_nsamples, 0.0);
-		for (size_t i = 0; i < fadc_hits.size(); ++i) {
-			const auto &hit = fadc_hits[i];
-			const size_t channel = static_cast<size_t>(hit->channel);
-			if (channel >= m_nchannels) {
-				throw std::runtime_error("Hit channel index is out of bounds");
-			}
-			for (size_t sample = 0; sample < fadc_waveforms[channel]->samples.size(); ++sample) {
-				waveform_data[channel * m_nsamples + sample] =
-					static_cast<double>(fadc_waveforms[channel]->samples[sample]);
+		std::vector<double> waveform_data(fadc_waveforms.size() * m_nsamples, 0.0); // [nchannels, nsamples]
+		for (size_t i = 0; i < fadc_waveforms.size(); ++i) {
+			std::transform(
+				fadc_waveforms[i]->samples.begin(), fadc_waveforms[i]->samples.end(),
+				waveform_data.begin() + i * m_nsamples, [](auto sample) { return static_cast<double>(sample); }
+			);
+		}
+
+		size_t total_hits = 0;
+		for (const auto &cluster : vtp_clusters) {
+			total_hits += cluster->hit_indices.size();
+		}
+
+		std::vector<double> hit_data(total_hits * 2, 0.0);		// charge, time
+		std::vector<double> geometry_data(total_hits * 2, 0.0); // column, row
+		std::vector<std::int64_t> cluster_data(total_hits, -1);
+
+		for (const auto *clus : vtp_clusters) {
+			for (size_t i = 0; i < clus->hit_indices.size(); ++i) {
+				size_t hit_index = clus->hit_indices[i];
+				hit_data[hit_index * 2] = clus->energies[i];
+				hit_data[hit_index * 2 + 1] = clus->times[i];
+
+				int channel = clus->channels[i];
+				auto [col, row] = m_service_geometry().getColRowFromBlock(channel);
+				geometry_data[hit_index * 2] = static_cast<double>(col);
+				geometry_data[hit_index * 2 + 1] = static_cast<double>(row);
+				cluster_data[hit_index] = static_cast<std::int64_t>(clus->id);
 			}
 		}
 
-		// multiple hits per channel, hence 1 node per hit
-		std::vector<double> hit_data;
-		std::vector<double> geometry_data;
-		hit_data.reserve(fadc_hits.size() * 2);		 // charge, time
-		geometry_data.reserve(fadc_hits.size() * 2); // column, row
-
-		for (auto &hit : fadc_hits) {
-			const size_t channel = static_cast<size_t>(hit->channel);
-			if (channel >= m_nchannels) {
-				throw std::runtime_error("Hit channel index is out of bounds");
+		size_t edge_count = 0;
+		for (const auto *clus : vtp_clusters) {
+			if (clus->id == -1) {
+				continue; // skip dummy clusters
 			}
-			hit_data.emplace_back(static_cast<double>(hit->charge));
-			hit_data.emplace_back(static_cast<double>(hit->time));
-
-			const auto [column, row] = m_service_geometry().getColRowFromBlock(channel);
-			geometry_data.emplace_back(static_cast<double>(column));
-			geometry_data.emplace_back(static_cast<double>(row));
+			edge_count += clus->channels.size() - 1; // edges from seed to other hits
 		}
 
-		std::vector<std::int64_t> edge_index_data;
-		for (size_t cluster_index = 0; cluster_index < vtp_clusters.size(); ++cluster_index) {
-			auto &channels = vtp_clusters[cluster_index]->channels;
-			auto seed_channel = channels[0];
-			for (size_t i = 1; i < channels.size(); ++i) {
-				auto channel = channels[i];
-				edge_index_data.push_back(static_cast<std::int64_t>(seed_channel));
-				edge_index_data.push_back(static_cast<std::int64_t>(channel));
+		std::vector<std::int64_t> edge_index_data(edge_count * 2, -1); // [2, nedges]
+
+		size_t edge_pos = 0;
+		for (const auto *clus : vtp_clusters) {
+			if (clus->id == -1) {
+				continue; // skip dummy clusters
+			}
+			auto seed_channel = clus->channels[0];
+			for (size_t i = 1; i < clus->channels.size(); ++i) {
+				auto channel = clus->channels[i];
+				edge_index_data[edge_pos * 2] = static_cast<std::int64_t>(seed_channel);
+				edge_index_data[edge_pos * 2 + 1] = static_cast<std::int64_t>(channel);
+				edge_pos++;
 			}
 		}
 
-		std::vector<std::int64_t> cluster_data(fadc_hits.size(), -1);
-		for (size_t cluster_index = 0; cluster_index < vtp_clusters.size(); ++cluster_index) {
-			for (size_t hit_index : vtp_clusters[cluster_index]->hit_indices) {
-				cluster_data[hit_index] = cluster_index;
-			}
-		}
-
-		cnpy::npy_save(output_dir / "waveforms.npy", waveform_data.data(), {m_nchannels, m_nsamples});
-		cnpy::npy_save(output_dir / "hits.npy", hit_data.data(), {fadc_hits.size(), 2});
-		cnpy::npy_save(output_dir / "geometry.npy", geometry_data.data(), {fadc_hits.size(), 2});
+		cnpy::npy_save(output_dir / "waveforms.npy", waveform_data.data(), {fadc_waveforms.size(), m_nsamples});
+		cnpy::npy_save(output_dir / "hits.npy", hit_data.data(), {total_hits, 2});
+		cnpy::npy_save(output_dir / "geometry.npy", geometry_data.data(), {total_hits, 2});
 		cnpy::npy_save(output_dir / "edge_index.npy", edge_index_data.data(), {2, edge_index_data.size() / 2});
-		cnpy::npy_save(output_dir / "clusters.npy", cluster_data.data(), {fadc_hits.size()});
+		cnpy::npy_save(output_dir / "clusters.npy", cluster_data.data(), {total_hits});
 	}
 
 protected:
