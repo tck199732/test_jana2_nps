5826984bf545207293eb0779fbd20462d96be0f6
Kin <chikin.tam@proton.me>
2026-08-31 21:10:03 -0400

    update hit index and edge index to account for dropped hits.

--- src/nps/io/NpyWriterProcessor.hpp
+++ src/nps/io/NpyWriterProcessor.hpp
@@ -77,18 +77,30 @@ public:
 			);
 		}
 
+		// not the same as fadc_hits.size() since some hits are dropped during clustering,
+		// e.g. not matching vtp seed.
 		size_t total_hits = 0;
 		for (const auto &cluster : vtp_clusters) {
 			total_hits += cluster->hit_indices.size();
 		}
 
-		std::vector<double> hit_data(total_hits * 2, 0.0);		// charge, time
-		std::vector<double> geometry_data(total_hits * 2, 0.0); // column, row
-		std::vector<std::int64_t> cluster_data(total_hits, -1);
+		size_t edge_count = 0;
+		for (const auto *clus : vtp_clusters) {
+			edge_count += clus->hit_indices.size() - 1; // edges from seed to other hits
+		}
 
+		std::vector<double> hit_data(total_hits * 2, 0.0);			   // charge, time
+		std::vector<double> geometry_data(total_hits * 2, 0.0);		   // column, row
+		std::vector<std::int64_t> cluster_data(total_hits, -1);		   // object id (0,.., nclusters-1)
+		std::vector<std::int64_t> cluster_type(total_hits, -1);		   // -1 = UNKNOWN, see cluster_type enum
+		std::vector<std::int64_t> edge_index_data(edge_count * 2, -1); // [2, nedges]
+
+		size_t hit_index_offset = 0;
+		size_t edge_pos = 0;
 		for (const auto *clus : vtp_clusters) {
 			for (size_t i = 0; i < clus->hit_indices.size(); ++i) {
-				size_t hit_index = clus->hit_indices[i];
+
+				size_t hit_index = hit_index_offset + i;
 				hit_data[hit_index * 2] = clus->energies[i];
 				hit_data[hit_index * 2 + 1] = clus->times[i];
 
@@ -97,30 +109,24 @@ public:
 				geometry_data[hit_index * 2] = static_cast<double>(col);
 				geometry_data[hit_index * 2 + 1] = static_cast<double>(row);
 				cluster_data[hit_index] = static_cast<std::int64_t>(clus->id);
+				cluster_type[hit_index] = static_cast<std::int64_t>(clus->type);
 			}
-		}
 
-		size_t edge_count = 0;
-		for (const auto *clus : vtp_clusters) {
-			if (clus->id == -1) {
-				continue; // skip dummy clusters
-			}
-			edge_count += clus->channels.size() - 1; // edges from seed to other hits
-		}
-
-		std::vector<std::int64_t> edge_index_data(edge_count * 2, -1); // [2, nedges]
-
-		size_t edge_pos = 0;
-		for (const auto *clus : vtp_clusters) {
-			if (clus->id == -1) {
-				continue; // skip dummy clusters
-			}
-			auto seed_channel = clus->channels[0];
+			auto src_id = hit_index_offset;
 			for (size_t i = 1; i < clus->channels.size(); ++i) {
-				auto channel = clus->channels[i];
-				edge_index_data[edge_pos * 2] = static_cast<std::int64_t>(seed_channel);
-				edge_index_data[edge_pos * 2 + 1] = static_cast<std::int64_t>(channel);
+				auto des_id = hit_index_offset + i;
+				edge_index_data[edge_pos * 2] = static_cast<std::int64_t>(src_id);
+				edge_index_data[edge_pos * 2 + 1] = static_cast<std::int64_t>(des_id);
 				edge_pos++;
+			}
+
+			hit_index_offset += clus->hit_indices.size();
+		}
+
+		// sanity check: all hits are assigned to valid indices
+		for (const auto *clus : vtp_clusters) {
+			if (clus->id == -1) {
+				throw std::runtime_error("Cluster id is -1, which is invalid.");
 			}
 		}
 
@@ -128,7 +134,8 @@ public:
 		cnpy::npy_save(output_dir / "hits.npy", hit_data.data(), {total_hits, 2});
 		cnpy::npy_save(output_dir / "geometry.npy", geometry_data.data(), {total_hits, 2});
 		cnpy::npy_save(output_dir / "edge_index.npy", edge_index_data.data(), {2, edge_index_data.size() / 2});
-		cnpy::npy_save(output_dir / "clusters.npy", cluster_data.data(), {total_hits});
+		cnpy::npy_save(output_dir / "cluster_index.npy", cluster_data.data(), {total_hits});
+		cnpy::npy_save(output_dir / "cluster_type.npy", cluster_type.data(), {total_hits});
 	}
 
 protected:
